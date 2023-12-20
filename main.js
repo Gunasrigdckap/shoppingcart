e5db5aa5fc1ec3083e885f3e5214b03a3186e266
DCKAP <gunasrigdckap@gmail.com>
2023-12-20 12:54:23 +0530

    push

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict"
+
+
+var product_name = document.getElementById("Product_input");
+var amount = document.getElementById("amount_input");
+var sub_btn = document.getElementById("submit_Btn");
+console.log(product_name,amount,sub_btn);
+var Invalid_para1=document.getElementById("check1");
+var Invalid_para2=document.getElementById("check2");
+
+sub_btn.addEventListener()
